1dc6057e7c455bf42619e72c3481ed22aa986b9e
SJA <jeongah8815@naver.com>
2024-07-02 16:20:51 +0900

    폴더 구조 세팅

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <h2>hello</h2>
+        </div>
+    );
+}
+
+export default App;
