7544e564e01e7572860b2dc012665a4cc47f6a72
SJA <jeongah8815@naver.com>
2024-07-03 09:27:42 +0900

    header, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import './styles/App.css';
+import Header_center from "./components/header_center.jsx";
+import Header_left from "./components/header_left.jsx";
+import Footer from './components/footer.jsx';
 
 function App() {
     return (
         <div className="App">
+            <Header_center />
             <h1>Hello</h1>
             <h2>hello</h2>
+            <Footer/>
         </div>
     );
 }
