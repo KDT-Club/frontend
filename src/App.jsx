66223b25f0386e245594248683a15d82d75604e0
SJA <jeongah8815@naver.com>
2024-07-03 14:40:48 +0900

    마이페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './styles/App.css';
 import Header_center from "./components/Header_center.jsx";
 import Header_left from "./components/Header_left.jsx";
@@ -6,6 +6,21 @@ import Footer from './components/Footer.jsx';
 import Mypage from "./pages/mypage/Mypage.jsx";
 
 function App() {
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        let vw = window.innerWidth * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+        document.documentElement.style.setProperty("--vw", `${vw}px`);
+    }
+
+    useEffect(() => {
+        setScreenSize();
+        window.addEventListener('resize', setScreenSize);
+
+
+        return () => window.removeEventListener('resize', setScreenSize);
+    }, []);
+
     return (
         <div className="App">
             <Header_center />
