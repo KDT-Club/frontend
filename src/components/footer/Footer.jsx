2ba76688f67d4b73ef3694ae408347613a0ce932
eunseopi <asxzqw12@naver.com>
2024-07-09 16:53:12 +0900

    Footer, Header 경로 변경

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import './footer.css';
+import { FaRegBookmark } from 'react-icons/fa';
+import { PiUsersThree } from "react-icons/pi";
+import { IoHomeOutline } from "react-icons/io5";
+import { LuUserSquare } from "react-icons/lu";
+import { Link, useLocation } from 'react-router-dom';
+
+function Footer() {
+    const location = useLocation();
+    const [activeMenu, setActiveMenu] = useState(location.pathname);
+
+    return (
+        <div className="Footer">
+            <Menu
+                to="/main"
+                Icon={IoHomeOutline}
+                title="홈"
+                iconStyle={{fontSize:"30px", marginTop:"2px"}}
+                textStyle={{marginTop:"2px"}}
+                isActive={activeMenu === "/main"}
+                onClick={() => setActiveMenu("/home")}
+            />
+            <Menu
+                to="/community"
+                Icon={PiUsersThree}
+                title="커뮤니티"
+                iconStyle={{fontSize:"33px", marginLeft:"6px"}}
+                textStyle={{marginTop:"2px"}}
+                isActive={activeMenu === "/community"}
+                onClick={() => setActiveMenu("/community")}
+            />
+            <Menu
+                to="/myclub"
+                Icon={FaRegBookmark}
+                title="내 동아리"
+                iconStyle={{fontSize:"27px", marginTop:"1px", marginLeft:"12px"}}
+                textStyle={{marginTop:"6px"}}
+                isActive={activeMenu === "/myclub"}
+                onClick={() => setActiveMenu("/myclub")}
+            />
+            <Menu
+                to="/mypage"
+                Icon={LuUserSquare}
+                title="마이페이지"
+                iconStyle={{fontSize:"30px", marginLeft:"15px"}}
+                textStyle={{marginTop:"5px"}}
+                isActive={activeMenu === "/mypage"}
+                onClick={() => setActiveMenu("/members")}
+            />
+        </div>
+    )
+}
+
+function Menu({ to, Icon, iconStyle, title, textStyle, isActive, onClick }) {
+    const activeStyle = isActive ? { color: '#597CA5' } : { color: 'darkgray' };
+
+    return (
+        <div className="menu">
+            <Link to={to} style={activeStyle} onClick={onClick}>
+                <Icon style={{ ...iconStyle, ...activeStyle }} />
+                <p style={{ ...textStyle, ...activeStyle }}>{title}</p>
+            </Link>
+        </div>
+    )
+}
+
+export default Footer;
