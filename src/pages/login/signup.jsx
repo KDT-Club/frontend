5d7cdffdc3d14d088bd9d57afeff6a8a6c34984e
SJA <jeongah8815@naver.com>
2024-07-11 09:18:35 +0900

    API 설계 - 복붙

--- src/pages/login/signup.jsx
+++ src/pages/login/signup.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import Header_center from "../../components/header/Header_center.jsx";
-import './login_styles/signup.css';
-import '../../styles/App.css';
+import './login_styles/signup.css'
+import '../../styles/App.css'
 import back from '../../images/shape.png'
 
 function Modal({onClose}) {
@@ -10,8 +10,8 @@ function Modal({onClose}) {
         <div className="modal-overlay">
             <div className="modal-content">
                 <div className="modal-header">
-                <p>가입이 완료되었습니다!</p>
-                <p>로그인 화면으로 돌아갑니다.</p>
+                    <p>가입이 완료되었습니다!</p>
+                    <p>로그인 화면으로 돌아갑니다.</p>
                 </div>
                 <hr/>
                 <button onClick={onClose}>OK</button>
@@ -23,11 +23,11 @@ function Modal({onClose}) {
 
 function signup () {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [id, setId] = React.useState('');
+    const [username, setUserName] = React.useState('');
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [name, setName] = React.useState('');
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [major, setMajor] = React.useState('');
+    const [department, setDepartment] = React.useState('');
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [password, setPassword] = React.useState('');
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -38,6 +38,8 @@ function signup () {
     const navigate = useNavigate();
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [showModal, setShowModal] = React.useState(false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [phone, setPhone] = React.useState('');
 
     const handleDuplicateCheck = () => {
         setDuplicateId(true);
@@ -46,6 +48,33 @@ function signup () {
     const handleBackClick = () => {
         window.history.back();
     }
+
+
+    // 회원가입 API 나중에 개발.
+    // const handleSignUp = async () => {
+    //     try {
+    //         if(password !== confirmPassword) {
+    //             alert('비밀번호가 일치하지 않습니다.');
+    //             return;
+    //         }
+    //
+    //         const response = await fetch('https://?/signup', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({name, phone, username, department, password })
+    //         });
+    //
+    //         if(response.status === 200) {
+    //             navigate('/login');
+    //         }
+    //     }catch(error){
+    //         console.log('회원가입 중 에러 발생:',error);
+    //         alert('회원가입 중 에러가 발생하였습니다.');
+    //     }
+    // }
+
 
     const handleSignup = (e) => {
         e.preventDefault();
@@ -64,7 +93,7 @@ function signup () {
                     <img src={back} alt="back"/>
                 </button>
                 <div className="signup-header-center">
-                <Header_center/>
+                    <Header_center/>
                 </div>
             </div>
             <div className="signup-title">
@@ -82,25 +111,35 @@ function signup () {
                     />
                 </div>
                 <div className="input-group">
+                    <label htmlFor="phone">핸드폰 번호</label>
+                    <input
+                        id="phone"
+                        type="text"
+                        placeholder="010-0000-0000"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </div>
+                <div className="input-group">
                     <label htmlFor="student-major">학과</label>
                     <input
                         id="student-major"
                         type="text"
                         placeholder="학과를 입력해주세요."
-                        value={major}
-                        onChange={(e) => setMajor(e.target.value)}
+                        value={department}
+                        onChange={(e) => setDepartment(e.target.value)}
                     />
                 </div>
                 <div className="input-group">
                     <label htmlFor="student-id">학번</label>
                     <div className="input-with-button">
-                    <input
-                        id="student-id"
-                        type="id"
-                        placeholder="학번을 입력해주세요."
-                        value={id}
-                        onChange={(e) => setId(e.target.value)}
-                    />
+                        <input
+                            id="student-id"
+                            type="id"
+                            placeholder="학번을 입력해주세요."
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
                         <button onClick={handleDuplicateCheck} className="duplicate-check-btn">중복확인</button>
                     </div>
                 </div>
@@ -130,7 +169,7 @@ function signup () {
                 </div>
                 <button type="submit" className="signup-btn" onClick={handleSignup}>가입하기</button>
                 {showModal && (
-                    <Modal onClose={handleModalClose} />
+                    <Modal onClose={handleModalClose}/>
                 )}
             </div>
         </div>
